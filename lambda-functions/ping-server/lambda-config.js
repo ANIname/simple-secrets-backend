d8732e1adb46affc3ad3568a1c387ab12a714316
KiiDii <kiidii.aniname@gmail.com>
2022-04-22 21:48:03 +0300

    👌 IMPROVE: updated test lambda functions

--- lambda-functions/ping-server/lambda-config.js
+++
@@ -1,8 +0,0 @@
-exports['ping-server'] = {
-  description: 'Simple function to check if requests are working',
-  handler:     'lambda-functions/ping-server/index.handler',
-
-  events: [
-    { http: { path: 'ping', method: 'ANY', cors: { origin: '*' } } },
-  ],
-};
---
+++ lambda-functions/server/test/lambda-config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  description: 'Simple function to test code',
+  url:         true,
+};
